fc3d52fa00110d0123a133e3cd379e5d1cb8664f
Dragmine <dan@thebanners.uk>
2021-10-05 18:08:47 +0100

    Updated setup.py to install all required modules

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import os
 os.system("python -m venv .venv")
-os.system(".venv/bin/python -m pip install")
+os.system(".venv/bin/python -m pip install discord.py")
+os.system(".venv/bin/python -m pip install dislash.py")
+os.system(".venv/bin/python -m pip install aiofiles")
+os.system("clear")
+print("Finish seting up required files for the bot to run!")
+print(f"run {os.path.dirname(__file__)}/.venv/bin/python bot.py to run the bot")  # noqa
